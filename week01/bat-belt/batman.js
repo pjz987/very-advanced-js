15c3fdc6f779b3cf67b369f5bd101bafa399a9e6
Pete Jones <pwj2012@gmail.com>
2020-07-28 13:02:35 -0700

    batman fail

---
+++ week01/bat-belt/batman.js
@@ -0,0 +1,11 @@
+const _ = {}
+
+_.compose = (...functions) => {
+    for (let i=0; i<functions.length; i++) {
+        if (i + 1 >= functions.length) {
+
+        }
+        let func = functions[i]
+        
+    }
+}
--- week01/bat-belt/test/test-robin.js
+++ week01/bat-belt/test/test-robin.js
@@ -1,2 +1,4 @@
-import { describe, Try } from 'riteway'
-import exercises from '../robin'
+// import { describe, Try } from 'riteway'
+// import exercises from '../robin'
+
+console.log('test')
